4511096961d989617b32a55211816b56fd2265ce
Dylan Pineda <dyspinedaa@gmail.com>
2026-02-03 21:06:47 -0500

    Sexto commit

--- program.js
+++ program.js
@@ -1,8 +1,9 @@
-var map = L.map('map').setView([4.6056791895607345, -74.0700973611025], 13);
+var map = L.map('map').setView([4.605700577951365, -74.07010809045742], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([4.6056791895607345, -74.0700973611025]).addTo(map).bindPopup("SINCHI").openPopup();
+var marker = L.marker([4.605700577951365, -74.07010809045742]).addTo(map).bindPopup('<b>Sede SINCHI</b><br>Instituto Amazónico de Investigaciones Científicas').openPopup();
+
